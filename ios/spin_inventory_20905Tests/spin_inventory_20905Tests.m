e076839fd14456a68031bd7a8199632ce8eb30c2
Crowdbotics <team@crowdbotics.com>
2020-09-30 21:40:28 +0000

    Initial commit.

---
+++ ios/spin_inventory_20905Tests/spin_inventory_20905Tests.m
@@ -0,0 +1,30 @@
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <React/RCTLog.h>
+#import <React/RCTRootView.h>
+
+#define TIMEOUT_SECONDS 600
+#define TEXT_TO_LOOK_FOR @"Welcome to React"
+
+@interface spin_inventory_20905 : XCTestCase
+
+@end
+
+@implementation spin_inventory_20905
+
+- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
+{
+  if (test(view)) {
+    return YES;
+  }
+  for (UIView *subview in [view subviews]) {
+    if ([self findSubviewInView:subview matching:test]) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+
+@end
